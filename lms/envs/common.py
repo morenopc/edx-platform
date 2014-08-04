52ab6ca324f1effb59f8dd9107e511f42a90b3d6
Moreno Cunha <moreno.pinheiro@gmail.com>
2014-08-04 18:52:36 +0000

    Translate certificate names

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1317,8 +1317,8 @@ REGISTRATION_EXTRA_FIELDS = {
 }
 
 ########################## CERTIFICATE NAME ########################
-CERT_NAME_SHORT = "Certificate"
-CERT_NAME_LONG = "Certificate of Achievement"
+CERT_NAME_SHORT = "Certificado"
+CERT_NAME_LONG = u"Certificado de Conclusão"
 
 ###################### Grade Downloads ######################
 GRADES_DOWNLOAD_ROUTING_KEY = HIGH_MEM_QUEUE
